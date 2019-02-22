2baf1adab9725418387b2fb731bc523b378abc40
sunxiaojie <sunxiaojie@pinpianyi.com>
2019-02-22 19:25:44 +0800

    循环爬虫抓取数据

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import json
+import math
 
 def load_pic(url):
     response = requests.get(url)
@@ -9,16 +10,17 @@ def load_pic(url):
 
 # res = requests.get('http://www.nipic.com/topic/show_27202_1.html')
 
-cookiess = {
-    "JSESSIONID": "A29CDB2595A07B0F04BACA5265D5730A",
-    "pageNo": "3",
-    "pageSize": "10"
-}
-ssrequest = requests.session()
-# requests.utils.add_dict_to_cookiejar(ssrequest.cookies, cookiess)
-payload = {'serviceType':'51','workObjectNumber':'d0c79a34b0104fbdbf7118f07973559b','workflowNumber':'d0c79a34b0104fbdbf7118f07973559b','stuCode':'BJ2056545','classCode':'VIPTVS190045','subject':'微信群4＋1服务动作反馈'}
-url = "http://bpm.xdf.cn/studyservice/todo/toItemPage"
-req = requests.get(url, cookies=cookiess, params=payload)
-soup = BeautifulSoup(req.text, "html.parser")
-token = soup.find(type="hidden")
-print(token.attrs['value'])
+# maxPage = 9
+# while maxPage>=1:
+#     print(maxPage)
+#     maxPage-=1
+
+print(1.0+1)
+print(1+1)
+print(1-2)
+print(1.0-2)
+print(1.0*2)
+print(1.0/2)
+print(int(1/2))
+print(math.ceil(3.0/2))
+print(math.ceil(22/10))
--- xindongfang.py
+++ xindongfang.py
@@ -1,58 +1,71 @@
 import requests
 from bs4 import BeautifulSoup
+import math
+import re
 
-cookiess = {
-    "JSESSIONID": "1500956D798AEE42A8DC25F5CA0C28ED",
-    "pageNo": "3",
-    "pageSize": "10"
+COOKIES = {
+    "JSESSIONID": "40BCACEC26775928C4014CC762EB7423"
 }
-payload = {'pageNo': 2}
 continueStr = '21日'
+pageUrl = "http://bpm.xdf.cn/studyservice/todo/list?subject=教师4＋1质检&orderByStr=expect_end_time%20desc&=2"
+pageParam = {'pageNo': 1}
 
-url = "http://bpm.xdf.cn/studyservice/todo/list?subject=教师4＋1质检&orderByStr=expect_end_time%20desc&=2"
-req = requests.get( url, cookies=cookiess, params=payload)
+
+def classinfo(page):
+    pageParam['pageNo'] = page
+    req = requests.get(pageUrl, cookies=COOKIES, params=pageParam)
+    response = BeautifulSoup(req.text)
+    stus = response.find_all(class_='Work_list1')
+    for stu in stus:
+        start_date = stu.find(class_='startBtn')
+        print(re.split(r'[年|月|日]', start_date.string))
+        if start_date.string.find(continueStr) > 0:
+            continue
+        print(start_date.string)
+        classInfo = stu.find(href="javascript:void(0);")
+        str = classInfo['onclick'].replace('\'', '').split(',')
+        serviceType = str[0].replace('dealwith(', '')
+        workObjectNumber = str[1]
+        workflowNumber = str[2]
+        stuCode = str[3]
+        classCode = str[4]
+        classParam = {'serviceType': serviceType, 'workObjectNumber': workObjectNumber,
+                      'workflowNumber': workflowNumber, 'stuCode': stuCode, 'classCode': classCode}
+        print(classParam)
+        # classDetail = requests.get('http://bpm.xdf.cn/studyservice/todo/toItemPage', params=classParam, cookies=cookiess)
+        # classDetailSoup = BeautifulSoup(classDetail.text, "html.parser")
+        # tokenDiv = classDetailSoup.find(type="hidden")
+        # classParam.pop('serviceType')
+        # classParam.setdefault('token', tokenDiv.attrs['value'])
+        # classParam.setdefault('lesson_type', '1')
+        # classParam.setdefault('homework_setting', '1')
+        # classParam.setdefault('inclass_test', '1')
+        # classParam.setdefault('homework_feedback', '1')
+        # classParam.setdefault('inclass_feedback', '1')
+        # classParam.setdefault('group_interaction', '1')
+        # classParam.setdefault('remark', '')
+        # finish = requests.get('http://bpm.xdf.cn/workflow/wechatFeedBackNew/finishWechatFeedBackNew', params=classParam, cookies=cookiess)
+        # print(finish)
+        # print(classParam)
+        # print("finish")
+
+
+pageList = requests.get(pageUrl, cookies=COOKIES, params=pageParam)
 cookies = requests.cookies.RequestsCookieJar()
-cookies.update(req.cookies)
-soup = BeautifulSoup(req.text)
-print(soup.find_all(class_="saucy"))
-stus = soup.find_all(class_='Work_list1')
-for stu in stus:
-    startBtn = stu.find(class_='startBtn')
-    if startBtn.string.find(continueStr)>0:
-        continue
-    print(startBtn.string)
-    classInfo = stu.find(href="javascript:void(0);")
-    str = classInfo['onclick'].replace('\'', '').split(',')
-    serviceType = str[0].replace('dealwith(','')
-    workObjectNumber = str[1]
-    workflowNumber = str[2]
-    stuCode = str[3]
-    classCode = str[4]
-    classParam = {'serviceType':serviceType, 'workObjectNumber':workObjectNumber, 'workflowNumber':workflowNumber, 'stuCode':stuCode, 'classCode':classCode}
-    print(classParam)
-    classDetail = requests.get('http://bpm.xdf.cn/studyservice/todo/toItemPage', params=classParam, cookies=cookiess)
-    classDetailSoup = BeautifulSoup(classDetail.text, "html.parser")
-    tokenDiv = classDetailSoup.find(type="hidden")
-    classParam.pop('serviceType')
-    classParam.setdefault('token', tokenDiv.attrs['value'])
-    classParam.setdefault('lesson_type', '1')
-    classParam.setdefault('homework_setting', '1')
-    classParam.setdefault('inclass_test', '1')
-    classParam.setdefault('homework_feedback', '1')
-    classParam.setdefault('inclass_feedback', '1')
-    classParam.setdefault('group_interaction', '1')
-    classParam.setdefault('remark', '')
-    finish = requests.get('http://bpm.xdf.cn/workflow/wechatFeedBackNew/finishWechatFeedBackNew', params=classParam, cookies=cookiess)
-    print(finish)
-    print(classParam)
-    print("finish")
-    # print(classParam)
+cookies.update(pageList.cookies)
+soup = BeautifulSoup(pageList.text)
+searchNum = int(soup.find(class_="searchNum").string)
+maxPage = math.ceil(searchNum/10)
+print('总共有', searchNum, '条，', maxPage, '页数据待处理。。。。。。。。。。。。。。。。。。')
+while maxPage >= 1:
+    classinfo(maxPage)
+    print('处理第', maxPage, '页成功！！！！！！！！！！！！！！！！！！！')
+    maxPage -= 1
 
 
-    # print(classCode)
 
-# print(soup)
-# print(req.text)
+
+
 
 
 
